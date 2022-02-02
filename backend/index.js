a504b0588cd33b1a9681c113d6e73d5e4ec87495
bradleyhewitt <bradleywh01@gmail.com>
2022-02-02 12:16:11 -0800

    Make backend provide a teams list to "/teams" (example implementation for providing data)

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,34 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const teams = {
+    teams_list: [
+        {
+            name: 'Kings',
+            wins: 18,
+            losses: 34,
+        },
+        {
+            name: 'Warriors',
+            wins: 39,
+            losses: 13
+        },
+        {
+            name: 'Pelicans',
+            wins: 19,
+            losses: 32
+        }
+    ]
+}
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send("Backend Landing");
+});
+
+app.get('/teams', (req, res) => {
+    res.send(teams);
 });
 
 app.listen(port, () => {
