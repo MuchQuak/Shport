b5033b41ea1de446439a85d0061c60a80ae382e7
Bradley Hewitt <40677305+bradleyhewitt@users.noreply.github.com>
2022-02-02 15:26:22 -0800

    Require fixes, should be cors not express

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
-const cors = require('express');
+const cors = require('cors');
 const express = require('express');
-
-
 const app = express();
 const port = 5000;
-
 app.use(cors());
 app.use(express.json());
 
