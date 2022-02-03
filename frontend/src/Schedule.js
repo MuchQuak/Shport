4753853e3c9d71e7992b2f9d9c009078834ecaba
bradleyhewitt <bradleywh01@gmail.com>
2022-02-02 19:02:43 -0800

    Make NBAGame a react component

--- frontend/src/Schedule.js
+++ frontend/src/Schedule.js
@@ -1,64 +1,12 @@
 import React from 'react';
 import './App.css';
 import './GameSchedule.css';
+import './NBAGame.js'
+import NBAGame from "./NBAGame";
 
 function Schedule(props) {
     const games = props.gameData.map((game, index) => {
-        var clock_data = game.clock.toString().trim();
-        function ready(data) {
-            return data !== "";
-        }
-        function over() {
-            return !game.halftime && !ready(clock_data) && game.currentQtr === 4 && game.endPeriod && game.activated
-        }
-        function halftime() {
-            return game.halftime || (!ready(clock_data) && game.currentQtr === 2 && game.endPeriod && game.activated)
-        }
-        const clock = function() {
-            if (over()) {
-                return (<p>Final Score</p>)
-            }
-            if (halftime()) {
-                return (<p><b>Halftime</b></p>)
-            }
-            if (!ready(clock_data) || !game.activated){
-                return (<p>{game.startTimeEST}</p>);
-            }
-            return (<p><b>{clock_data} - {game.currentQtr} of {game.maxQtr}</b></p>)
-        }
-        const score = function(score) {
-            if (score === "" || !game.activated) {
-                return (null);
-            }
-            return (<p className='score'>{score}</p>)
-        }
-        const logo = function(abbreviation) {
-            var logo = 'https://www.nba.com/.element/img/1.0/teamsites/logos/teamlogos_500x500/' + abbreviation.toLowerCase() + '.png';
-            return (<div className='logo-container'><img src={logo} alt='logo'/></div>)
-        }
-        const name_record = function (team, record) {
-            return (<div className='game-name-record'><p className='game-team-name'>{team}</p><p className='game-record'>{record}</p></div>)
-        }
-        return (
-            <div className='game'>
-                <div className='game-header'>
-                    {clock()}
-                </div>
-                <div className='game-data'>
-                    <div className='game-left'>
-                        {logo(game.home)}
-                        {name_record(game.home, game.home_record)}
-                        {score(game.home_score)}
-                    </div>
-                    <div className='game-right'>
-                        {logo(game.away)}
-                        {name_record(game.away, game.away_record)}
-                        {score(game.away_score)}
-                    </div>
-                </div>
-                <p className='game-footer'>{game.arena}</p>
-            </div>
-        );
+        return (<NBAGame game={game} />);
     });
     return (
         <div className='schedule'>{games}</div>
