02baad793a12eace7dc6d476cd004d7f121ee68a
hrend <hrendon@calpoly.edu>
2022-02-04 20:47:52 -0800

    minor fixes to signup page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,11 @@ import App from './App';
 //import Login from './Login';
 import SignUp from './SignUp';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 /*
    -- Login Page Testing --
    Need to uncomment this but comment out the ReactDom.render from above
    Also need to call [npm install react-bootstrap bootstrap@5.1.3] on the frontend to install react-bootstrap*/
 
-ReactDOM.render(<SignUp />, document.getElementById('root'));
+//ReactDOM.render(<SignUp />, document.getElementById('root'));
