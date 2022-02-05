4af5dec177e526232179eefe94650002379729c8
hrend <hrendon@calpoly.edu>
2022-02-04 19:56:17 -0800

    added SignUp template

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-//import Login from './Login';
+//import App from './App';
+import Login from './Login';
+import SignUp from './SignUp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 /*
    -- Login Page Testing --
    Need to uncomment this but comment out the ReactDom.render from above
    Also need to call [npm install react-bootstrap bootstrap@5.1.3] on the frontend to install react-bootstrap*/
 
-//ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<SignUp />, document.getElementById('root'));
