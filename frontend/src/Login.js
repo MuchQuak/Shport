9c713c9da1dce0bb3258b452317de3322887ecf7
bradleyhewitt <bradleywh01@gmail.com>
2022-02-04 18:28:43 -0800

    Make login UI match theme, code cleanup

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './Login.css';
 
 
@@ -13,12 +9,10 @@ export default function Login() {
   const [password, setPassword] = useState("");
 
   function validateForm() {
-        let emailValidation = validateEmail();
-        let passwordValidation = validatePassword();
-
+    let emailValidation = validateEmail();
+    let passwordValidation = validatePassword();
     return emailValidation && passwordValidation;
   }
-
 
   function validatePassword(){
     if(password.length > 0){
@@ -39,45 +33,23 @@ export default function Login() {
   }
 
   return (
-      <Container fluid="false">
-          <Row>
-              <Col lg={12} md={8} ><br></br></Col>
-          </Row>
-          <Row>
-              <Col lg={12} ><br></br></Col>
-          </Row>
-          <Row>
-    <Col lg={{ span: 4, offset: 4 }}>
-    <div className="Login">
-    <h1 className="websiteName">SPORTS</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="passwordForm" size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button className="submitButton" block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-        <Button className="signUp" block size="lg" type="button">
-          Sign Up
-        </Button>
-      </Form>
-    </div>
-    </Col>
-  </Row>
-    </Container>
+      <div className='login-content'>
+          <h1 className="welcome">Welcome to Sports Dashboard</h1>
+          <div className="login">
+              <h1 className="websiteName">Log In</h1>
+              <Form onSubmit={handleSubmit}>
+                  <Form.Group className="emailForm" size="lg" controlId="email">
+                      <Form.Label>Email</Form.Label>
+                      <Form.Control autoFocus type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                  </Form.Group>
+                  <Form.Group className="passwordForm" size="lg" controlId="password">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                  </Form.Group>
+                  <Button className="login-button" id="login" block size="lg" type="submit" disabled={!validateForm()}>Login</Button>
+                  <Button className="login-button" id="signup" block size="lg" type="button">Sign Up</Button>
+              </Form>
+          </div>
+      </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-//import Login from './Login';
+import Login from './Login';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 /*
    -- Login Page Testing --
    Need to uncomment this but comment out the ReactDom.render from above
    Also need to call [npm install react-bootstrap bootstrap@5.1.3] on the frontend to install react-bootstrap*/
 
-//ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'));
