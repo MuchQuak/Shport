0050695474410ffdce9397479c8d8616fe451d47
bradleyhewitt <bradleywh01@gmail.com>
2022-02-05 09:35:00 -0800

    Restore App as main page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,4 +13,4 @@ import SignUp from './SignUp';
    Need to uncomment this but comment out the ReactDom.render from above
    Also need to call [npm install react-bootstrap bootstrap@5.1.3] on the frontend to install react-bootstrap*/
 
-ReactDOM.render(<SignUp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
