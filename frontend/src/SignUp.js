e2ad7d457861d47b2c689baac1de128aa221db83
bradleyhewitt <bradleywh01@gmail.com>
2022-02-04 23:25:06 -0800

    Sign up fixes

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -7,8 +7,7 @@ import './SignUp.css';
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [firstName, setFirstName] = useState("");  
-  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
 
   function validateForm() {
     let emailValidation = validateEmail();
@@ -37,86 +36,34 @@ export default function SignUp() {
 
   return (
         <div className="signup-content">
-          <div className="signup">
-            <h1 className="signup-name">Sign Up!</h1>
-
-            <Form onSubmit={handleSubmit}>
-              <Form.Group className="firstnameForm" size="lg" controlId="firstname">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control
-                type="text"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-              </Form.Group>
-
-              <Form.Group className="lastnameForm" size="lg" controlId="lastname">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-              </Form.Group>
-
-
-              <Form.Group size="lg" controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  autoFocus
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </Form.Group>
-
-              <Form.Group className="passwordForm" size="lg" controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Form.Group>
-                
-              <p className="preferences">Preferences</p>
-
-              {['checkbox'].map((type) => (
-                <div key={`default-${type}`} className="mb-3">
-                  <Form.Check 
-                    type={type}
-                    label={`No Preferences`}
-                    id={`0`}
-                  />
-
-                  <Form.Check 
-                    type={type}
-                    label={`NBA`}
-                    id={`1`}
-                  />
-
-                  <Form.Check
-                    disabled
-                    type={type}
-                    label={`NFL`}
-                    id={`2`}
-                  />
-
-                  <Form.Check
-                    disabled
-                    type={type}
-                    label={`MLB`}
-                    id={`3`}
-                  />
-
-                </div>
-              ))}
-
-              <Button className="submit-button" id="signup-button" block size="lg" type="submit" disabled={!validateForm()}>Sign Up</Button>
-
-              <p className="have-account">Already registered <a href="#">sign in?</a></p>
-        </Form>
-      </div>
-    </div>
+            <div className="signup">
+                <h1 className="signup-name">Sign Up</h1>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="usernameForm" size="lg" controlId="username">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    </Form.Group>
+                    <Form.Group className="emailForm" size="lg" controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control autoFocus type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    </Form.Group>
+                    <Form.Group className="passwordForm" size="lg" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    </Form.Group>
+                    <p className="preferences">Preferences</p>
+                    {['checkbox'].map((type) => (
+                      <div key={`default-${type}`} className="mb-3">
+                          <Form.Check type={type} label={`No Preferences`} id={`0`}/>
+                          <Form.Check type={type} label={`NBA`} id={`1`}/>
+                          <Form.Check disabled type={type} label={`NFL`} id={`2`}/>
+                          <Form.Check disabled type={type} label={`MLB`} id={`3`}/>
+                      </div>
+                    ))}
+                    <Button className="submit-button" id="signup-button" block size="lg" type="submit" disabled={!validateForm()}>Sign Up</Button>
+                    <p className="have-account">Already registered? <a href="#">Sign in</a></p>
+                </Form>
+            </div>
+        </div>
   );
 }
