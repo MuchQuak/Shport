d0b81f1a36998d8a1895b3960b84c6a05c601fa8
hrend <hrendon@calpoly.edu>
2022-02-09 11:49:01 -0800

    changed order

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -9,9 +9,9 @@ export default function NavBar() {
               <Link to="/"><h1 className="header-text">Dashboard</h1></Link>
             </div>
             <div className="header-right">
-              <Link className="header-link" to="/Login" id='header-login'>Login</Link>
               <Link className="header-link" to="/About" id='header-about'>About Us</Link>
               <Link className="header-link" to="/ProjectTeam" id='header-team'>Our Team</Link>
+              <Link className="header-link" to="/Login" id='header-login'>Sign Out</Link>
             </div>
           </div>
       </div>
