21ae0ed05857cdbbcb4eec2f420b19d5486fb456
bradleyhewitt <bradleywh01@gmail.com>
2022-02-09 11:22:44 -0800

    Finish nav fixes

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
     return (
       <div className="header">
-        <Link to="/">
-          <h1 className="header-text">Dashboard</h1>
-        </Link>
           <div className="header-nav">
-            <Link className="header-link" to="/Login" id='header-login'>Login</Link>
-            <Link className="header-link" to="/About" id='header-about'>About Us</Link>
-            <Link className="header-link" to="/ProjectTeam" id='header-team'>Our Team</Link>
+            <div className="header-left">
+              <Link to="/"><h1 className="header-text">Dashboard</h1></Link>
+            </div>
+            <div className="header-right">
+              <Link className="header-link" to="/Login" id='header-login'>Login</Link>
+              <Link className="header-link" to="/About" id='header-about'>About Us</Link>
+              <Link className="header-link" to="/ProjectTeam" id='header-team'>Our Team</Link>
+            </div>
           </div>
       </div>
     );
