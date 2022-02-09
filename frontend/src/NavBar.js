484e914e6a31275a3eb0fdb1e4d244569a2c2dc1
rtblast70 <ryanj.tsai@gmail.com>
2022-02-09 11:08:24 -0800

    minor dashboard routing
    - clickable dashboard in navbar
    - removed home link in sub pages

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
     return (
       <div className="header">
-        <h1 className="header-text">Dashboard</h1>
+        <Link to="/">
+          <h1 className="header-text">Dashboard</h1>
+        </Link>
           <div className="header-nav">
             <Link className="header-link" to="/Login" id='header-login'>Login</Link>
             <Link className="header-link" to="/About" id='header-about'>About Us</Link>
