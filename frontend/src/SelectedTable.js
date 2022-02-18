f6847d6a86ade8307029a4a97c7aa2f04fc70153
bradleyhewitt <bradleywh01@gmail.com>
2022-02-17 19:35:46 -0800

    Add logos in preferences

--- frontend/src/SelectedTable.js
+++ frontend/src/SelectedTable.js
@@ -1,4 +1,5 @@
 import Button from "react-bootstrap/Button";
+import {getTeamLogo} from "./SportHandler";
 
 export default function SelectedTable(props) {
   if (!props || props.selectedData.length < 1){
@@ -7,7 +8,7 @@ export default function SelectedTable(props) {
     const rows = props.selectedData.map((row, index) => {
         return (
             <tr key={index}>
-                <td>{row.city} {row.name}</td>
+                <td><div className='logo-multiline-words'>{getTeamLogo(row.sport, row.code, null)}{row.city} {row.name}</div></td>
                 <td>{row.sport}</td>
                 <td>
                     <Button onClick={() => props.removeSelected(index)}>Remove</Button>
@@ -16,11 +17,13 @@ export default function SelectedTable(props) {
         );
     });
   return (
-    <table className='teamsTable'>
-        <p className='nomargin bold underline'>Selected</p>
-        <tbody>
-        {rows}
-        </tbody>
-    </table>
+      <>
+          <p className='nomargin bold underline'>Selected</p>
+          <table className='teamsTable'>
+              <tbody>
+              {rows}
+              </tbody>
+          </table>
+      </>
   );
 }
