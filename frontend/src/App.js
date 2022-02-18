79e5c1a4a95d7a849de6bf8d006e6be2f6191626
hrend <hrendon@calpoly.edu>
2022-02-18 15:59:30 -0800

    add prefs to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import {useLocation} from 'react-router-dom'; // Might need again for later
+import axios from 'axios';
 import './style/App.css';
 import NavBar from './NavBar';
 import Dashboard from "./Dashboard";
@@ -11,7 +12,29 @@ export default function App() {
         location.state.username = "[ Username ]";
         location.state.prefs = [];
     }
+    else{
+        getPrefs();
+    }
+
+    async function getPrefs(){
+        try {
+            const url = 'http://localhost:5000/preferences';
+            const response = await axios.post(url, {"username":location.state.username});
+            
+            if (response.status === 201){
+                location.state.prefs = response.data[0].prefs;
+                console.log(location.state.prefs);
+                return response.data;
+            }
+        }
+        catch (error){
+          console.log(error);
+          return error.data;
+        }
+      }
+
     const prefs = location.state.prefs;
+    
     return (
       <>
           <NavBar/>
