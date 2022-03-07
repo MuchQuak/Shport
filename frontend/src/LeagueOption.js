cd6bf40252c0ebffea8bd0aaaafc9b4724faad4b
bradleyhewitt <bradleywh01@gmail.com>
2022-03-07 11:22:32 -0800

    Settings work

---
+++ frontend/src/LeagueOption.js
@@ -0,0 +1,24 @@
+import {getLeagueLogo} from "./SportHandler";
+import './style/LeaguePreferenceSelector.css';
+
+export default function LeagueOption(props){
+    if (!props || !props.league || !props.click) {
+        return null;
+    }
+    const icon = getLeagueLogo(props.league);
+    const className = () => {
+        if (props.disabled === true) {
+            return ' league-option-inactive';
+        } else if (props.active === true) {
+            return ' league-option-active'
+        }
+        return ''
+    }
+    return (
+        <div className={'league-option noselect' + className()} onClick={props.click}>
+            <div className='logo-name-record'>
+                {icon}{props.league}
+            </div>
+        </div>
+    );
+}
--- frontend/src/Settings.js
+++ frontend/src/Settings.js
@@ -1,12 +1,13 @@
 import NavBar from './NavBar';
 import {useLocation} from "react-router-dom";
 import LeaguePreferenceSelector from "./LeaguePreferenceSelector";
+import {Accordion} from "react-bootstrap";
 
 function prefDisplay(prefs) {
     if (prefs && Object.keys(prefs).length > 0) {
-        return <p className='nomargin'>{JSON.stringify(prefs)}</p>;
+        return <p className='nomargin' style={{color:'#000000'}}>{JSON.stringify(prefs)}</p>;
     }
-    return <p className='nomargin'>No preferences found!</p>;
+    return <p className='nomargin' style={{color:'#000000'}}>No preferences found!</p>;
 }
 
 export default function Settings() {
@@ -23,8 +24,15 @@ export default function Settings() {
         <NavBar/>
         <div className='boxed'>
             <h1>Settings</h1>
-            <LeaguePreferenceSelector />
-            {prefDisplay(prefs)}
+            <LeaguePreferenceSelector prefs={prefs}/>
+            <Accordion style={{margin: '10px'}}>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>View Raw JSON</Accordion.Header>
+                    <Accordion.Body>
+                        {prefDisplay(prefs)}
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
         </div>
       </main>
     );
