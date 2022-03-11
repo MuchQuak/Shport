069b1a38fc21cfb963e1b91a8a2354eb64eb51f7
bradleyhewitt <bradleywh01@gmail.com>
2022-03-11 10:37:04 -0800

    Merge "About Us" and "Our Team" pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,6 @@ import './style/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import About from './About';
-import ProjectTeam from './ProjectTeam';
 import Login from './Login';
 import SignUp from './SignUp';
 import LeaguePreferences from './LeaguePreferences';
@@ -21,13 +20,11 @@ ReactDOM.render(
    <Routes>
      <Route path="/" element={<App />} />
      <Route path="About" element={<About />} />
-     <Route path="ProjectTeam" element={<ProjectTeam />} />
      <Route path="Login" element={<Login />} />
      <Route path="SignUp" element={<SignUp />} />
      <Route path="LeaguePreferences" element={<LeaguePreferences />} />
      <Route path="TeamPreferences" element={<TeamPreferences />} />
      <Route path="Settings" element={<Settings/>} />
-
    </Routes>
  </BrowserRouter>, 
    document.getElementById('root'
