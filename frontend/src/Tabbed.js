4b36d3760012ee84d98edc9ee6710ed035693211
bradleyhewitt <bradleywh01@gmail.com>
2022-03-13 16:52:38 -0700

    Move Tab to inside Tabbed.js

--- frontend/src/Tabbed.js
+++ frontend/src/Tabbed.js
@@ -1,6 +1,18 @@
 import "./style/tab.scss";
 import React from 'react';
-import Tab from "./Tab";
+
+export function Tab(props) {
+    if (!props || !props.title || !props.click) {
+        return null;
+    }
+    const icon = props.icon ? props.icon : null;
+    function className() {
+        return 'tab-title noselect ' + (props.active === true ? 'tab-active' : 'tab-inactive');
+    }
+    return (
+        <div className={className()} onClick={props.click}><div className='logo-name-record'>{icon}{props.title}</div></div>
+    );
+}
 
 export default function Tabbed(props) {
     const [currentTab, setCurrentTab] = React.useState(props.default ? props.default : 0);
