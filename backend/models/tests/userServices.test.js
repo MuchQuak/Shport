bc34e52e131ca760419789c63700a1598c068b8d
hrend <hrendon@calpoly.edu>
2022-04-06 12:44:06 -0700

    added more tests

--- backend/models/tests/userServices.test.js
+++ backend/models/tests/userServices.test.js
@@ -306,6 +306,7 @@ test("Logging in user -- no email Failure", async () => {
   expect(loginResult).toBeFalsy();
 });
 
+// --- TESTS DONE --- 
 
 test("Adding user w/o validation -- successful path", async () => {
   const user = {
@@ -313,15 +314,24 @@ test("Adding user w/o validation -- successful path", async () => {
     "email": "youngWizard@gmail.com",
     "password": "Sample%%44*5",
   };
+  let resultUser1 = {
+    username: user.username,
+    email: user.email,
+    _id: new mongoose.ObjectId("624ddbd7f9bbb3ab16c362ee"),
+    prefs: new mongoose.ObjectId("624ddbd7f9bbb3ab16c362ef"),
+    salt: '0ab6e8e2c911609a81101f83b9044267',
+    hash: '910ef94b7754121e6cd22da20c9f16d0f4778c07585c49b6ddab11c3fd60432260136e027ca6bb11e0bc85f1b97cd4fa7c812bf11f6124d7c56bac137594a067'
+  }; 
+  mockingoose(userModel).toReturn(resultUser1, 'save');
   const result = await userServices.signUpUser(user);
 
   expect(result).toBeTruthy();
   expect(result.username).toBe(user.username);
   expect(result.email).toBe(user.email);
-  expect(result.validPassword("Sample%%44*5")).toBeTruthy();
+  expect(result.validPassword("1")).toBeTruthy();
   expect(result).toHaveProperty("_id");
 });
-//-----------------------------
+
 test("Adding user w/o validation -- failure path with invalid id", async () => {
   const user = {
     "_id": "123",
@@ -329,12 +339,12 @@ test("Adding user w/o validation -- failure path with invalid id", async () => {
     "email": "youngWizard@gmail.com",
     "password": "Sample%%44*5",
   };
-
+  mockingoose(userModel).toReturn(false, 'save');
   const result = await userServices.signUpUser(user);
+
   expect(result).toBeFalsy();
 });
 
-// ---- TESTS DONE ----
 test("Adding user w/o validation -- failure path with already taken id", async () => {
   const user = {
     "username": "Harry Potter",
