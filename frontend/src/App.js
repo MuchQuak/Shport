1cb09d7f4fc205e8ff4c8615612c82fc8b40b7e7
bradleyhewitt <bradleywh01@gmail.com>
2022-04-13 11:11:40 -0700

    Fix console errors related to undefined auth token or username. Automatically redirect anyone without a JWT token to the login screen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,56 @@
 import "./style/app.scss";
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./dashboard/NavBar";
 import Dashboard from "./dashboard/Dashboard";
 import Settings from "./settings/Settings";
 import About from "./dashboard/about/About";
-import { getUsername, prefsQuery } from "./login-signup/UserHandler";
+import { prefsQuery, usernameQuery} from "./login-signup/UserHandler";
 import { useQuery } from "react-query";
 import { errorSuffix, loadingSuffix } from "./util/Util";
+import {useNavigate} from "react-router";
+
+const userModel = {
+  info: {
+    name: "Guest",
+  },
+  auth_token: "",
+  prefs: null,
+};
 
 export default function App(props) {
-  const [user, setUser] = useState({});
-  const [username, setUsername] = useState("Guest");
+  const [user, setUser] = useState(userModel);
+  const navigate = useNavigate();
   const auth_token = props.cookies.auth_token;
-  const nameQuery = useQuery(
-    ["username", auth_token],
-    () => getUsername(auth_token),
-    {
-      onSuccess: (data) => {
-        setUsername(data);
-      },
+  useEffect(() => {
+    if (auth_token === undefined || auth_token === "") {
+      navigate("/login");
+    } else {
+      console.log(auth_token);
     }
+  });
+  const nameQuery = useQuery(
+      ["username", auth_token],
+      () => usernameQuery(auth_token),
+      {
+        onSuccess: (data) => {
+          const tempUser = { ...user }
+          tempUser.info.name = data;
+          setUser(tempUser);
+        }
+      }
   );
   const prefQuery = useQuery(
-    ["prefs", auth_token],
-    () => prefsQuery(auth_token),
-    {
-      onSuccess: (data) => {
-        const temp = {
-          info: {
-            name: username,
-          },
-          auth_token: props.cookies.auth_token,
-          prefs: data,
-        };
-        setUser(temp);
-      },
-    }
+      ["prefs", auth_token],
+      () => prefsQuery(auth_token),
+      {
+        onSuccess: (data) => {
+          const tempUser = { ...user }
+          tempUser.auth_token = props.cookies.auth_token;
+          tempUser.prefs = data;
+          setUser(tempUser);
+        },
+      }
   );
   if (prefQuery.isLoading || nameQuery.isLoading) {
     return loadingSuffix("app");
@@ -46,16 +60,16 @@ export default function App(props) {
     return errorSuffix(nameQuery.error);
   }
   return (
-    <>
-      <NavBar user={user} removeCookie={props.removeCookie} />
-      <Routes>
-        <Route index element={<Dashboard user={user} />} />
-        <Route
-          path="settings"
-          element={<Settings user={user} setUser={setUser} />}
-        />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </>
+      <>
+        <NavBar user={user} removeCookie={props.removeCookie} />
+        <Routes>
+          <Route index element={<Dashboard user={user} />} />
+          <Route
+              path="settings"
+              element={<Settings user={user} setUser={setUser} />}
+          />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </>
   );
 }
--- frontend/src/login-signup/UserHandler.js
+++ frontend/src/login-signup/UserHandler.js
@@ -23,16 +23,15 @@ export async function setUserPrefs(user) {
   }
 }
 
-export async function getUsername(auth_token) {
-  try {
+export async function usernameQuery(auth_token) {
     const config = { headers: { authorization: `Bearer ${auth_token}` } };
-    return await axios
-      .get("http://localhost:5000/username", config)
-      .then((data) => data.data.username);
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
+    return await axios.get("http://localhost:5000/username", config)
+      .then((res) => {
+        if (res.status === 200) {
+          return res.data.username;
+        }
+        throw new Error("Error " + res.status + ": Could not retrieve username.");
+      });
 }
 
 export async function prefsQuery(auth_token) {
@@ -42,8 +41,6 @@ export async function prefsQuery(auth_token) {
     if (res.status === 201) {
       return res.data;
     }
-    throw new Error(
-      "Error " + res.status + ": Could not retrieve preferences."
-    );
+    throw new Error("Error " + res.status + ": Could not retrieve preferences.");
   });
 }
