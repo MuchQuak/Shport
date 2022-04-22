243f3d137b166092bc53bb55e74a8e9623e881ca
bradleyhewitt <bradleywh01@gmail.com>
2022-04-21 23:49:16 -0700

    Give articles a fresh new themed coat of paint. Add slight animations to some images.

--- frontend/src/dashboard/news/Article.js
+++ frontend/src/dashboard/news/Article.js
@@ -1,22 +1,37 @@
 import "../../style/article.scss";
+import {css, StyleSheet} from "aphrodite";
+import {useContext} from "react";
+import {ThemeContext} from "../../App";
+
+const styles = (th) => {
+    return StyleSheet.create({
+        article: {
+            color: th.base,
+            ":hover": {
+                color: th.border,
+            },
+        },
+    });
+}
 
 export default function Article(props) {
-  if (!props || !props.prefs || !props.news) {
-    return null;
-  }
-
-  return (
-    <div className="article">
-      <a href={props.news.url} target="_blank" rel="noreferrer">
-        <p className="article-title">{props.news.title}</p>
-        <img
-          className="article-img"
-          src={props.news.image}
-          alt={props.news.title}
-        />
-      </a>
-      <p className="article-date">{props.news.date}</p>
-      <p className="article-desc">{props.news.description}</p>
-    </div>
-  );
+    const { theme } = useContext(ThemeContext);
+    const styled = styles(theme);
+    if (!props || !props.prefs || !props.news) {
+        return null;
+    }
+    return (
+        <div className="article">
+          <a href={props.news.url} target="_blank" rel="noreferrer">
+            <p className={css(styled.article) + " article-title"}>{props.news.title}</p>
+            <img
+              className="article-img"
+              src={props.news.image}
+              alt={props.news.title}
+            />
+          </a>
+          <p className="article-date">{new Date(props.news.date).toLocaleString('en-US', {month: "long", day: "numeric", year: "numeric"})}</p>
+          <p className="article-desc">{props.news.description}</p>
+        </div>
+    );
 }
