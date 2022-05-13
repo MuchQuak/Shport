a7076942c21762a1b57dbd68a8a00eee883e3dad
bradleyhewitt <bradleywh01@gmail.com>
2022-05-13 10:27:51 -0700

    Attempt any cors origin

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 
 // Models
