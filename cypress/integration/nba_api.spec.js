38925b1e47f4ae00d12265521a1b4d59d4723e2b
hrend <hrendon@calpoly.edu>
2022-05-18 11:22:52 -0700

    test cypress

--- cypress/integration/nba_api.spec.js
+++ cypress/integration/nba_api.spec.js
@@ -4,3 +4,11 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+
+describe('My First Test', () => {
+    it('Visits the Kitchen Sink', () => {
+      cy.visit('https://example.cypress.io')
+    })
+  })
+  
