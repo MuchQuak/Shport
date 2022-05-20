facccb8765e7ab97c86bc55f24bb54549d452c75
hrend <hrendon@calpoly.edu>
2022-05-20 11:40:20 -0700

    cookie for e_to_e

--- cypress/integration/login_end_to_end.spec.js
+++ cypress/integration/login_end_to_end.spec.js
@@ -1,9 +1,11 @@
 // npx cypress open
+import 'cypress-react-selector';
 
 describe('End to End Login Test', () => {
     context('SUCCESSFUL login', ()=> {
         it('GIVEN I navigate to the login page', ()=>{
             cy.visit('https://shport-frontend.herokuapp.com/login');
+            cy.waitForReact(1000,'#root');
         })
         it('WHEN I enter a valid username and password and submit the form', ()=>{
             cy.intercept({
@@ -16,21 +18,12 @@ describe('End to End Login Test', () => {
                 cy.get('input[type="password"]').type("1");
                 cy.contains('Login').click();
             });
-
             cy.wait('@loginUser');
+            cy.getCookie('auth_token').should('have.property', 'value');
         });
-        
         it('THEN I am logged in and see my username', ()=>{
             cy.get('.nomargin.dropdown:last').click()
             cy.contains('Hello, aa');
-        });
-        
-        it('THEN ', () => {
-            cy.getCookies()
-            .should('have.length', 1)
-            .then((cookies) => {
-                expect(cookies[0]).to.have.property('name', 'session_id')
-            });
         });
     })
   });
